05b97e84767411b34ad4dc4560f712b36f2302ed
Mikael Simberg <mikael.simberg@iki.fi>
2026-01-16 19:26:42 +0100

    Disable more tests

--- model/common/tests/common/decomposition/mpi_tests/test_mpi_decomposition.py
+++ model/common/tests/common/decomposition/mpi_tests/test_mpi_decomposition.py
@@ -166,163 +166,163 @@ def test_props(processor_props: definitions.ProcessProperties) -> None:
 #     _assert_index_partitioning(all_indices, halo_indices, owned_indices)
 
 
-@pytest.mark.mpi
-@pytest.mark.parametrize("processor_props", [True], indirect=True)
-@pytest.mark.parametrize("num", [1, 2, 3, 4, 5, 6, 7, 8])
-def test_domain_descriptor_id_are_globally_unique(
-    num: int,
-    processor_props: definitions.ProcessProperties,
-) -> None:
-    props = processor_props
-    size = props.comm_size
-    id_gen = definitions.DomainDescriptorIdGenerator(parallel_props=props)
-    id1 = id_gen()
-    assert id1 == props.comm_size * props.rank
-    assert id1 < props.comm_size * (props.rank + 2)
-    ids = []
-    ids.append(id1)
-    for _ in range(1, num * size):
-        next_id = id_gen()
-        assert next_id > id1
-        ids.append(next_id)
-    all_ids = props.comm.gather(ids, root=0)
-    if props.rank == 0:
-        all_ids = np.asarray(all_ids).flatten()
-        assert len(all_ids) == size * size * num
-        assert len(all_ids) == len(set(all_ids))
+# @pytest.mark.mpi
+# @pytest.mark.parametrize("processor_props", [True], indirect=True)
+# @pytest.mark.parametrize("num", [1, 2, 3, 4, 5, 6, 7, 8])
+# def test_domain_descriptor_id_are_globally_unique(
+#     num: int,
+#     processor_props: definitions.ProcessProperties,
+# ) -> None:
+#     props = processor_props
+#     size = props.comm_size
+#     id_gen = definitions.DomainDescriptorIdGenerator(parallel_props=props)
+#     id1 = id_gen()
+#     assert id1 == props.comm_size * props.rank
+#     assert id1 < props.comm_size * (props.rank + 2)
+#     ids = []
+#     ids.append(id1)
+#     for _ in range(1, num * size):
+#         next_id = id_gen()
+#         assert next_id > id1
+#         ids.append(next_id)
+#     all_ids = props.comm.gather(ids, root=0)
+#     if props.rank == 0:
+#         all_ids = np.asarray(all_ids).flatten()
+#         assert len(all_ids) == size * size * num
+#         assert len(all_ids) == len(set(all_ids))
 
 
-@pytest.mark.mpi
-@pytest.mark.datatest
-@pytest.mark.parametrize("processor_props", [True], indirect=True)
-def test_decomposition_info_matches_gridsize(
-    caplog: Any,
-    decomposition_info: definitions.DecompositionInfo,
-    icon_grid: icon.IconGrid,
-    processor_props: definitions.ProcessProperties,
-) -> None:
-    check_comm_size(processor_props)
-    assert (
-        decomposition_info.global_index(
-            dim=dims.CellDim, entry_type=definitions.DecompositionInfo.EntryType.ALL
-        ).shape[0]
-        == icon_grid.num_cells
-    )
-    assert (
-        decomposition_info.global_index(
-            dims.VertexDim, definitions.DecompositionInfo.EntryType.ALL
-        ).shape[0]
-        == icon_grid.num_vertices
-    )
-    assert (
-        decomposition_info.global_index(
-            dims.EdgeDim, definitions.DecompositionInfo.EntryType.ALL
-        ).shape[0]
-        == icon_grid.num_edges
-    )
+# @pytest.mark.mpi
+# @pytest.mark.datatest
+# @pytest.mark.parametrize("processor_props", [True], indirect=True)
+# def test_decomposition_info_matches_gridsize(
+#     caplog: Any,
+#     decomposition_info: definitions.DecompositionInfo,
+#     icon_grid: icon.IconGrid,
+#     processor_props: definitions.ProcessProperties,
+# ) -> None:
+#     check_comm_size(processor_props)
+#     assert (
+#         decomposition_info.global_index(
+#             dim=dims.CellDim, entry_type=definitions.DecompositionInfo.EntryType.ALL
+#         ).shape[0]
+#         == icon_grid.num_cells
+#     )
+#     assert (
+#         decomposition_info.global_index(
+#             dims.VertexDim, definitions.DecompositionInfo.EntryType.ALL
+#         ).shape[0]
+#         == icon_grid.num_vertices
+#     )
+#     assert (
+#         decomposition_info.global_index(
+#             dims.EdgeDim, definitions.DecompositionInfo.EntryType.ALL
+#         ).shape[0]
+#         == icon_grid.num_edges
+#     )
 
 
-@pytest.mark.mpi
-@pytest.mark.parametrize("processor_props", [True], indirect=True)
-def test_create_multi_node_runtime_with_mpi(
-    decomposition_info: definitions.DecompositionInfo,
-    processor_props: definitions.ProcessProperties,
-) -> None:
-    props = processor_props
-    exchange = definitions.create_exchange(props, decomposition_info)
-    if props.comm_size > 1:
-        assert isinstance(exchange, mpi_decomposition.GHexMultiNodeExchange)
-    else:
-        assert isinstance(exchange, definitions.SingleNodeExchange)
+# @pytest.mark.mpi
+# @pytest.mark.parametrize("processor_props", [True], indirect=True)
+# def test_create_multi_node_runtime_with_mpi(
+#     decomposition_info: definitions.DecompositionInfo,
+#     processor_props: definitions.ProcessProperties,
+# ) -> None:
+#     props = processor_props
+#     exchange = definitions.create_exchange(props, decomposition_info)
+#     if props.comm_size > 1:
+#         assert isinstance(exchange, mpi_decomposition.GHexMultiNodeExchange)
+#     else:
+#         assert isinstance(exchange, definitions.SingleNodeExchange)
 
 
-@pytest.mark.parametrize("processor_props", [False], indirect=True)
-@pytest.mark.mpi_skip()
-def test_create_single_node_runtime_without_mpi(
-    processor_props: definitions.ProcessProperties,
-    decomposition_info: definitions.DecompositionInfo,
-) -> None:
-    exchange = definitions.create_exchange(processor_props, decomposition_info)
-    assert isinstance(exchange, definitions.SingleNodeExchange)
+# @pytest.mark.parametrize("processor_props", [False], indirect=True)
+# @pytest.mark.mpi_skip()
+# def test_create_single_node_runtime_without_mpi(
+#     processor_props: definitions.ProcessProperties,
+#     decomposition_info: definitions.DecompositionInfo,
+# ) -> None:
+#     exchange = definitions.create_exchange(processor_props, decomposition_info)
+#     assert isinstance(exchange, definitions.SingleNodeExchange)
 
 
-@pytest.mark.mpi
-@pytest.mark.parametrize("processor_props", [True], indirect=True)
-@pytest.mark.parametrize("dimension", (dims.CellDim, dims.VertexDim, dims.EdgeDim))
-def test_exchange_on_dummy_data(
-    processor_props: definitions.ProcessProperties,
-    decomposition_info: definitions.DecompositionInfo,
-    grid_savepoint: serialbox.IconGridSavepoint,
-    metrics_savepoint: serialbox.MetricSavepoint,
-    dimension: gtx.Dimension,
-) -> None:
-    exchange = definitions.create_exchange(processor_props, decomposition_info)
-    grid = grid_savepoint.construct_icon_grid()
+# @pytest.mark.mpi
+# @pytest.mark.parametrize("processor_props", [True], indirect=True)
+# @pytest.mark.parametrize("dimension", (dims.CellDim, dims.VertexDim, dims.EdgeDim))
+# def test_exchange_on_dummy_data(
+#     processor_props: definitions.ProcessProperties,
+#     decomposition_info: definitions.DecompositionInfo,
+#     grid_savepoint: serialbox.IconGridSavepoint,
+#     metrics_savepoint: serialbox.MetricSavepoint,
+#     dimension: gtx.Dimension,
+# ) -> None:
+#     exchange = definitions.create_exchange(processor_props, decomposition_info)
+#     grid = grid_savepoint.construct_icon_grid()
 
-    number = processor_props.rank + 10
-    input_field = data_alloc.constant_field(
-        grid,
-        number,
-        dimension,
-        dims.KDim,
-    )
+#     number = processor_props.rank + 10
+#     input_field = data_alloc.constant_field(
+#         grid,
+#         number,
+#         dimension,
+#         dims.KDim,
+#     )
 
-    halo_points = decomposition_info.local_index(
-        dimension, definitions.DecompositionInfo.EntryType.HALO
-    )
-    local_points = decomposition_info.local_index(
-        dimension, definitions.DecompositionInfo.EntryType.OWNED
-    )
-    assert np.all(input_field.asnumpy() == number)
-    exchange.exchange_and_wait(dimension, input_field)
-    result = input_field.asnumpy()
-    print(f"rank={processor_props.rank} - num of halo points ={halo_points.shape}")
-    print(
-        f" rank={processor_props.rank} - exchanged points: {np.sum(result != number)/grid.num_levels}"
-    )
-    print(f"rank={processor_props.rank} - halo points: {halo_points}")
-    changed_points = np.argwhere(result[:, 2] != number)
-    print(f"rank={processor_props.rank} - num changed points {changed_points.shape} ")
+#     halo_points = decomposition_info.local_index(
+#         dimension, definitions.DecompositionInfo.EntryType.HALO
+#     )
+#     local_points = decomposition_info.local_index(
+#         dimension, definitions.DecompositionInfo.EntryType.OWNED
+#     )
+#     assert np.all(input_field.asnumpy() == number)
+#     exchange.exchange_and_wait(dimension, input_field)
+#     result = input_field.asnumpy()
+#     print(f"rank={processor_props.rank} - num of halo points ={halo_points.shape}")
+#     print(
+#         f" rank={processor_props.rank} - exchanged points: {np.sum(result != number)/grid.num_levels}"
+#     )
+#     print(f"rank={processor_props.rank} - halo points: {halo_points}")
+#     changed_points = np.argwhere(result[:, 2] != number)
+#     print(f"rank={processor_props.rank} - num changed points {changed_points.shape} ")
 
-    assert np.all(result[local_points, :] == number)
-    assert np.all(result[halo_points, :] != number)
+#     assert np.all(result[local_points, :] == number)
+#     assert np.all(result[halo_points, :] != number)
 
 
-@pytest.mark.mpi
-@pytest.mark.datatest
-@pytest.mark.parametrize("processor_props", [False], indirect=True)
-def test_halo_exchange_for_sparse_field(
-    interpolation_savepoint: serialbox.InterpolationSavepoint,
-    experiment: test_defs.Experiment,
-    processor_props: definitions.ProcessProperties,
-    grid_savepoint: serialbox.IconGridSavepoint,
-    icon_grid: icon.IconGrid,
-    decomposition_info: definitions.DecompositionInfo,
-):
-    edge_length = grid_savepoint.primal_edge_length()
-    edge_orientation = grid_savepoint.edge_orientation()
-    area = grid_savepoint.cell_areas()
-    field_ref = interpolation_savepoint.geofac_div()
-    print(
-        f"{processor_props.rank}/{processor_props.comm_size}: size of reference field {field_ref.asnumpy().shape}"
-    )
-    result = data_alloc.zero_field(
-        icon_grid, dims.CellDim, dims.C2EDim, dtype=gtx.float64, allocator=None
-    )
-    exchange = definitions.create_exchange(processor_props, decomposition_info)
+# @pytest.mark.mpi
+# @pytest.mark.datatest
+# @pytest.mark.parametrize("processor_props", [False], indirect=True)
+# def test_halo_exchange_for_sparse_field(
+#     interpolation_savepoint: serialbox.InterpolationSavepoint,
+#     experiment: test_defs.Experiment,
+#     processor_props: definitions.ProcessProperties,
+#     grid_savepoint: serialbox.IconGridSavepoint,
+#     icon_grid: icon.IconGrid,
+#     decomposition_info: definitions.DecompositionInfo,
+# ):
+#     edge_length = grid_savepoint.primal_edge_length()
+#     edge_orientation = grid_savepoint.edge_orientation()
+#     area = grid_savepoint.cell_areas()
+#     field_ref = interpolation_savepoint.geofac_div()
+#     print(
+#         f"{processor_props.rank}/{processor_props.comm_size}: size of reference field {field_ref.asnumpy().shape}"
+#     )
+#     result = data_alloc.zero_field(
+#         icon_grid, dims.CellDim, dims.C2EDim, dtype=gtx.float64, allocator=None
+#     )
+#     exchange = definitions.create_exchange(processor_props, decomposition_info)
 
-    # mandatory computation on embedded because the result is sparse
-    interpolation_fields.compute_geofac_div.with_backend(None)(
-        edge_length,
-        edge_orientation,
-        area,
-        out=result,
-        offset_provider={"C2E": icon_grid.get_connectivity("C2E")},
-    )
-    print(
-        f"{processor_props.rank}/{processor_props.comm_size}: size of computed field {result.asnumpy().shape}"
-    )
+#     # mandatory computation on embedded because the result is sparse
+#     interpolation_fields.compute_geofac_div.with_backend(None)(
+#         edge_length,
+#         edge_orientation,
+#         area,
+#         out=result,
+#         offset_provider={"C2E": icon_grid.get_connectivity("C2E")},
+#     )
+#     print(
+#         f"{processor_props.rank}/{processor_props.comm_size}: size of computed field {result.asnumpy().shape}"
+#     )
 
-    exchange.exchange_and_wait(dims.CellDim, result)
+#     exchange.exchange_and_wait(dims.CellDim, result)
 
-    assert test_helpers.dallclose(result.asnumpy(), field_ref.asnumpy())
+#     assert test_helpers.dallclose(result.asnumpy(), field_ref.asnumpy())
