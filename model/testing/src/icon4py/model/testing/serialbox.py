bd8b164ecac9702bd6aa6e1778926f8851be7f58
Mikael Simberg <mikael.simberg@iki.fi>
2026-04-29 09:26:46 +0200

    Fix typo in reading z_gradh_exner field

--- model/testing/src/icon4py/model/testing/serialbox.py
+++ model/testing/src/icon4py/model/testing/serialbox.py
@@ -1642,7 +1642,7 @@ class NonHydroExitEdgeDiagnosticsUpdateVnSavepoint(IconSavepoint):
         return self._get_field("z_theta_v_e", dims.EdgeDim, dims.KDim)
 
     def z_gradh_exner(self):
-        return self._get_field("z_gradd_exner", dims.EdgeDim, dims.KDim)
+        return self._get_field("z_gradh_exner", dims.EdgeDim, dims.KDim)
 
     def vn(self):
         return self._get_field("vn_new", dims.EdgeDim, dims.KDim)
